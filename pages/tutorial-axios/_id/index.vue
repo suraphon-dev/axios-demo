bf0779aa91f8d122aa55ab4979c514129c43f5e7
suraphon <suraphon.k@merudy.com>
2021-07-03 15:59:29 +0700

    update connect api.

---
+++ pages/tutorial-axios/_id/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h2>{{ data.title }}</h2>
+        <div v-html="data.description"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id,
+            data: "",
+        }
+    },
+    methods: {
+        getDescriptionNews() {
+            this.$axios.$get(`http://www.server-rudydev.com/admin-project/api/news/${this.id}`).then(response => {
+                this.data = response
+            })
+        }
+    },
+    mounted() {
+        this.getDescriptionNews()
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    margin: 25px;
+}
+</style>
---
+++ pages/tutorial-axios/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="container">
+            <h2>
+                <u>Picture Api News</u>
+            </h2>
+            <img v-for="(item,index) in news" :key="index" :src="item.image" @click="openNews(item.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            news: []
+        }
+    },
+    methods: {
+        getNews() {
+            this.$axios.$get('http://www.server-rudydev.com/admin-project/api/news').then(response => {
+                this.news = response
+            })
+        },
+        openNews(id) {
+            this.$router.push(`tutorial-axios/${id}`)
+        }
+    },
+    mounted() {
+        this.getNews()
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px;
+    border-bottom: 1px solid;
+    padding-bottom: 10px;
+}
+img {
+    margin: 10px 0px;
+    width: 500px;
+    cursor: pointer;
+}
+</style>
