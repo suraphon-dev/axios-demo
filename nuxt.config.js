a29173fa3cdb15a511e85ef8351aa37e45a4bc9d
suraphon <suraphon.k@merudy.com>
2021-07-09 14:26:40 +0700

    update tutorial-css

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,16 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "stylesheet", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" },
+        { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" }
+    ],
+    script: [
+        { src: "https://code.jquery.com/jquery-3.4.1.slim.min.js", type: "text/javascript" },
+        { src: "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", type: "text/javascript" },
+        { src: "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", type: "text/javascript" }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
---
+++ pages/tutorial-css/index.vue
@@ -0,0 +1,120 @@
+<template>
+    <div>
+        <!-- tutorai css : contact -->
+        <div class="container-title">
+            <h2>My Title</h2>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, incidunt.</h4>
+        </div>
+        <div class="container-contact">
+            <div class="card-contact" v-for="(item,index) in 3" :key="index">BOX</div>
+        </div>
+        <hr />
+
+        <!-- tutorai css : box -->
+        <div class="container-title">
+            <h2>My Box</h2>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, incidunt.</h4>
+        </div>
+        <div class="container-box">
+            <div class="card-box" v-for="(item,index) in 6" :key="index">BOX</div>
+        </div>
+        <hr />
+
+        <!-- tutorai css : slider // import plugin bootstrap 4 -->
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div
+                    class="carousel-item"
+                    :class="{ 'active' : index == 0 }"
+                    v-for="(item,index) in banner"
+                    :key="index"
+                >
+                    <img class="d-block w-100" :src="item.image" />
+                </div>
+            </div>
+            <a
+                class="carousel-control-prev"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="prev"
+            >
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a
+                class="carousel-control-next"
+                href="#carouselExampleControls"
+                role="button"
+                data-slide="next"
+            >
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            banner: [],            
+        }
+    },
+    methods: {
+        gatBanner() {
+            this.$axios.$get('http://www.server-rudydev.com/admin-project/api/banner').then(response => {
+                this.banner = response
+            })
+        }
+    },
+    mounted() {
+        this.gatBanner()
+    }
+}
+</script>
+
+<style scoped>
+/* tutorai css : contact */
+.container-title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+}
+.container-contact {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: hsla(0, 0%, 85.9%, 0.1);
+    height: 50vh;
+}
+
+.card-contact {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    width: 200px;
+    height: 200px;
+}
+
+/* tutorai css : box */
+.container-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 20px;
+}
+.card-box {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    background: rgb(83, 83, 250);
+    color: white;
+    width: 400px;
+    height: 400px;
+    margin: 20px;
+}
+</style>
