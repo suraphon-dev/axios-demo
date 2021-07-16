686b13e3bd82f9a3eb03ada5bbccc4865848b1f7
suraphon <suraphon.k@merudy.com>
2021-07-16 11:53:44 +0700

    component google-map

---
+++ pages/tutorial-map/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container-map">
+        <h2>Google Map</h2>
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.565204712783!2d100.5352308148554!3d13.805064399776315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29c733087ea5b%3A0xe7cb6981e6d8dc1d!2sThe%20Siam%20Cement%20Public%20Company%20Limited!5e0!3m2!1sen!2sth!4v1626409500967!5m2!1sen!2sth"
+            width="800"
+            height="550"
+            style="border:0;"
+            allowfullscreen
+            loading="lazy"
+        ></iframe>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container-map {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
